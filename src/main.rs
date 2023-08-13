10b942f6dc39a1b3b8fa84b646032bf7c157ad5e
m11o <m11o@hey.jp>
2023-08-14 00:28:50 +0900

    implement dairy command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod dairy_manager;
+mod editor;
+mod git;
+
+use chrono::Local;
+
+use dairy_manager::DairyManager;
+use editor::Editor;
+use crate::git::Git;
+
 fn main() {
-    println!("Hello, world!");
+    let now = Local::now().date_naive();
+    let dairy_manager = DairyManager::new(&now);
+    let file_path = dairy_manager.build_file_path();
+    let ref_file_path = file_path.as_str();
+
+    let editor = Editor::new("nvim");
+    let status = editor.open(ref_file_path);
+    if status.success() {
+        Git::add(ref_file_path);
+        Git::commit(commit_message(ref_file_path).as_str());
+    } else {
+        println!("Failed to open file.");
+    }
+}
+
+fn commit_message(file_path: &str) -> String {
+    let now_datetime = Local::now();
+    let mut message = file_path.to_string();
+    message.push_str(format!(": {}", now_datetime.format("%Y-%m-%d %H:%M:%S")).as_str());
+    message
 }
