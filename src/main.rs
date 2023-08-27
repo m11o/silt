740536c1bfdb8423d5197cdc76b7a0a77c69e7d6
m11o <m11o@hey.jp>
2023-08-28 07:58:54 +0900

    refactor commit process commit処理をリファクタ

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod git;
 
 use chrono::Local;
 use std::env;
-use std::path::Path;
+use std::path::{PathBuf};
 
 use dairy_manager::DairyManager;
 use editor::Editor;
@@ -19,15 +19,11 @@ fn main() {
     let editor = Editor::new("nvim");
     let status = editor.open(ref_file_path);
     if status.success() {
-        let current_dir = env::current_dir().expect("Failed to get current directory.");
-        let current_path_instance = Path::new(current_dir.to_str().unwrap());
-        let root_dir = env::var("SILT_PROJECT_ROOT_PATH").expect("Failed to load project root path from env");
-        let root_path_instance = Path::new(root_dir.as_str());
-
-        env::set_current_dir(&root_path_instance).expect("Failed to set current directory.");
-        Git::add(ref_file_path);
-        Git::commit(commit_message(ref_file_path).as_str());
-        env::set_current_dir(&current_path_instance).expect("Failed to set current directory.");
+        fn git_add_and_commit_dairy_file(file_path: &str) {
+            Git::add(file_path);
+            Git::commit(commit_message(file_path).as_str());
+        }
+        change_current_to_root_dir(git_add_and_commit_dairy_file, ref_file_path);
     } else {
         println!("Failed to open file.");
     }
@@ -38,4 +34,24 @@ fn commit_message(file_path: &str) -> String {
     let mut message = file_path.to_string();
     message.push_str(format!(": {}", now_datetime.format("%Y-%m-%d %H:%M:%S")).as_str());
     message
+}
+
+fn change_current_to_root_dir<F>(function: F, file_name: &str) where F: Fn(&str) -> () {
+    env::set_current_dir(root_dir().as_path()).expect("Failed to set current directory.");
+    function(file_name);
+    env::set_current_dir(current_dir().as_path()).expect("Failed to set current directory.");
+}
+
+fn current_dir() -> PathBuf {
+    match env::current_dir() {
+        Ok(path) => path,
+        Err(_) => panic!("Failed to get current directory."),
+    }
+}
+
+fn root_dir() -> PathBuf {
+    match env::var("SILT_PROJECT_ROOT_PATH") {
+        Ok(path) => PathBuf::from(path),
+        Err(_) => panic!("Failed to load project root path from env"),
+    }
 }
